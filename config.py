1ebd97f714e61ce11e02cfa2d22b148f1be4148d
Massav0 <167071648+Massav0@users.noreply.github.com>
2026-03-28 12:55:18 +0100

    Add configuration settings for the Flask app

---
+++ config.py
@@ -0,0 +1,8 @@
+# Configuration settings for the Flask app
+
+class Config:
+    SECRET_KEY = 'your_secret_key'
+    DEBUG = True
+    TESTING = False
+    DATABASE_URI = 'sqlite:///app.db'
+    # Add any other configuration settings you need
